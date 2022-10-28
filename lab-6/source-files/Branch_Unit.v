f65d3bb045b6b8d30a0a2d61d680783d70e446a1
Kushaz Sehgal <kushaz.sehgal@gmail.com>
2022-10-28 15:50:57 +0530

    jay is dumb

--- lab-6/source-files/Branch_Unit.v
+++ lab-6/source-files/Branch_Unit.v
@@ -8,7 +8,7 @@
 ////////////////////////////////////////////////////////
 
 // Branch Unit to compute the next instruction based on branch instructions
-module branch_unit (
+module Branch_Unit (
     input [31:0] nextPC,
     input [25:0] label0,
     input [15:0] label1,
@@ -23,17 +23,17 @@ module branch_unit (
     // Stores the 26-bit and 16-bit sign-extended labels
     wire [31:0] extendLabel0, extendLabel1;
     // Stores output of M1 and M2 respectively.
-    wire [31:0] M1out, M2out;
+    wire [31:0] label_jump_addr, final_jump_addr;
     // Stores output of AND1
     wire isJump;
 
     assign extendLabel0 = {{6{label0[25]}}, label0};
     assign extendLabel1 = {{16{label1[15]}}, label1};
 
-    and AND1 (isJump, branch, validJump);
+    assign isJump = branch & validJump;
 
-    mux_32b_2_1 M1 (.a0(extendLabel0), .a1(extendLabel1), .sel(lblSel), .out(M1out));
-    mux_32b_2_1 M2 (.a0(M1out), .a1(rsAddr), .sel(jumpAddr), .out(M2out));
-    mux_32b_2_1 M3 (.a0(nextPC), .a1(M2out), .sel(isJump), .out(nextAddr));
+    mux_32b_2_1 label_jump_sel (.a0(extendLabel0), .a1(extendLabel1), .sel(lblSel), .out(label_jump_addr));
+    mux_32b_2_1 choose_jump_1 (.a0(Label), .a1(rsAddr), .sel(jumpAddr), .out(final_jump_addr));
+    mux_32b_2_1 choose_jump_2 (.a0(nextPC), .a1(final_jump_addr), .sel(isJump), .out(nextAddr));
     
 endmodule
